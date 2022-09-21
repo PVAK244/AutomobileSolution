c2b0fe435e4a237bcea3828819b39036e2efac66
PVAK244 <khoapvade150321@fpt.edu.vn>
2022-09-22 01:15:38 +0700

    Add project files.

---
+++ AutomobileLibrary/DataAccess/MyStockContext.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace AutomobileLibrary.DataAccess
+{
+    public partial class MyStockContext : DbContext
+    {
+        public MyStockContext()
+        {
+        }
+
+        public MyStockContext(DbContextOptions<MyStockContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Car> Cars { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("server =(local); database = MyStock;uid=sa;pwd=123456;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "Vietnamese_CI_AS");
+
+            modelBuilder.Entity<Car>(entity =>
+            {
+                entity.Property(e => e.CarId).HasColumnName("CarID");
+
+                entity.Property(e => e.CarName)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Manufacturer)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Price).HasColumnType("money");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
